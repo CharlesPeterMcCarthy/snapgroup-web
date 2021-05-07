55aaa9acfba2e34756e3bdce818c11dcd5e97a8a
Charles McCarthy <charles@studybundles.com>
2021-05-07 17:08:20 +0100

    Generate Device Id

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'snapgroup';
+
+  public title = 'SnapGroup';
+  public isSetup: boolean;
+  public deviceId: string;
+  public username: string;
+
+  public constructor() {
+    this.deviceId = localStorage.getItem('deviceId');
+    this.username = localStorage.getItem('username');
+    this.isSetup = !!(this.deviceId && !this.username);
+    console.log(2, this.deviceId);
+
+    if (!this.isSetup) {
+      this.setDeviceId();
+    }
+  }
+
+  private setDeviceId = (): void => {
+    this.deviceId = this.deviceId || uuid();
+    localStorage.setItem('deviceId', this.deviceId);
+  }
+
 }
