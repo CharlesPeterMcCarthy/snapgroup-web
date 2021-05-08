5170e3824c82ed6f12b6a7855c4783163aae7039
Charles McCarthy <charles@studybundles.com>
2021-05-08 17:20:27 +0100

    Updated Environment And Angular JSON

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,23 @@
 export const environment = {
-  production: true
+  production: true,
+  brand: 'SnapGroup',
+  s3Bucket: {
+    access: {
+      accessKeyId: 'AKIAQ74LYQAJRD3P4DVO',
+      secretAccessKey: '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',
+      region: 'eu-west-1'
+    },
+    name: 'snapgroup-snaps'
+  },
+  awsConfig: {
+    API: {
+      endpoints: [
+        {
+          name: 'dev-SnapGroups-ApiGatewayRestApi',
+          endpoint: 'https://zf00mmynaj.execute-api.eu-west-1.amazonaws.com/dev',
+          region: 'eu-west-1'
+        }
+      ]
+    }
+  }
 };
